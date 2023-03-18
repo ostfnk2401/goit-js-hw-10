49f5f4246491678ecf685ae6fee9f8fe652e97bf
ostfnk2401 <117888795+ostfnk2401@users.noreply.github.com>
2023-03-18 15:06:23 +0000

    Deploying to gh-pages from @ ostfnk2401/goit-js-hw-09@9c8f8301024a9d46da998094d6b8d6fcb5170704 🚀

---
+++ 01-color-switcher.e5913bc4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){n||(t.disabled=!0,n=setInterval(a,1e3))})),e.addEventListener("click",(function(){clearInterval(n),n=null,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.e5913bc4.js.map
